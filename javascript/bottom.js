510f322335bfaee009e2cd168bdff39e6b2fbee8
Damien Dart <damiendart@pobox.com>
2013-11-25 12:43:19 +0000

    Move JavaScript stuff.

---
+++ javascript/bottom.js
@@ -0,0 +1,19 @@
+// Some JavaScript for Obtain Corn Hoop's Tumblr theme.
+//
+// Copyright (C) 2013 Damien Dart, <damiendart@pobox.com>.
+// This file is distributed under the MIT licence. For more information,
+// please refer to the accompanying "LICENCE" file.
+
+selectnav("selectnav", { autoselect: false,
+    label: "&#x2605;&#x2605;&#x2605; NAVIGATION &#x2605;&#x2605;&#x2605;" });
+if (navigator.appVersion.indexOf("Mac") == -1) {
+  var site_title = document.getElementsByClassName("site-title");
+  for (var i = 0; i < site_title.length; i++) {
+    site_title[i].className += " js-site-title-windows-hack";
+  }
+}
+var relative_dates = document.getElementsByClassName("js-relative-date");
+for (var i = 0; i < relative_dates.length; i++) {
+  relative_dates[i].innerHTML =
+      relative_dates[i].getAttribute("data-relative-date");
+}
---
+++ javascript/top.js
@@ -0,0 +1,9 @@
+// Some JavaScript for Obtain Corn Hoop's Tumblr theme.
+//
+// Copyright (C) 2013 Damien Dart, <damiendart@pobox.com>.
+// This file is distributed under the MIT licence. For more information,
+// please refer to the accompanying "LICENCE" file.
+
+// HACK: Prevent "SelectNav.js"-related FOUC.
+document.documentElement.className += "js";
+try{Typekit.load();}catch(e){}
